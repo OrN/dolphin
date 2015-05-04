65cfb05c1f3c71995a8fc564f0e8005d24ffd433
OrNoX <preston.s1993@gmail.com>
2015-05-04 03:43:50 -0400

    forgot datalength in log

--- Source/Core/Core/HW/EXI_Channel.cpp
+++ Source/Core/Core/HW/EXI_Channel.cpp
@@ -166,7 +166,7 @@ void CEXIChannel::RegisterMMIO(MMIO::Mapping* mmio, u32 base)
 
 				// Schedule transfer complete for the future
 				CoreTiming::ScheduleEvent(delayTime, et_transfer_complete, (u64)m_ChannelId);
-				WARN_LOG(EXPANSIONINTERFACE, "EXICHANNEL(%u)_DMACONTROL clock:%uHz, delay:%uns", m_ChannelId, getClockRate(),
+				WARN_LOG(EXPANSIONINTERFACE, "EXICHANNEL(%u)_DMACONTROL clock:%uHz, data_length:%u, delay:%uns", m_ChannelId, getClockRate(),
 						dataLength, delayTime);
 			}
 		})
